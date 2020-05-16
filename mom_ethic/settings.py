7829d3ecde45e86f7fe72e684b624636a6ab33c7
Yaroslav <prdt@bk.ru>
2020-05-16 07:58:37 +0300

    privetsadsa

--- mom_ethic/settings.py
+++ mom_ethic/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '//'
+LOGIN_REDIRECT_URL = '/'
